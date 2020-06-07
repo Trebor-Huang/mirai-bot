5ae705aa951c74add6c8bcc588394e11eb5c20a1
Trebor Huang <2300936257@qq.com>
2020-06-07 12:10:15 +0800

    FIX PLUGIN ORDER BUG
    The original implementation assumed that plugin priorities are distinct.
    Fixed.

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ logging.basicConfig(format="[%(levelname)s] %(name)s \t- %(message)s")
 
 plugins_path = pathlib.Path(__file__).parent.absolute().glob('plugins/*.py')
 plugins_modules = [importlib.import_module("plugins." + pp.stem) for pp in plugins_path]
-plugins_list = sorted([p for m in plugins_modules if hasattr(m, "plugins") for p in m.plugins], reverse=True)
+plugins_list = sorted([p for m in plugins_modules if hasattr(m, "plugins") for p in m.plugins], key=lambda x:x[0], reverse=True)
 
 bot = botcore.Bot(int(sys.argv[1]), auth_key=sys.argv[2], plugins=[p for i, p in plugins_list])
 bot.connect()
